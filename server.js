ed45593bcfe8604f4e508ddaa9614b791c45c7d2
unknown <gustavosoftex@gmail.com>
2026-03-26 11:00:19 -0300

    POST e GET de usuarios pronto

--- server.js
+++ server.js
@@ -2,8 +2,28 @@ const express = require("express")
 
 const app = express()
 
+app.use(express.json())
+
+const usuarios = []
+
 app.get("/api",(req,res) => { 
     res.json({message:"Hello Word!"})
+})
+
+app.post("/cadastrar",(req,res)=>{
+    const {nome, idade} = req.body
+
+    usuarios.push({
+        nome,
+        idade
+    })
+
+    res.json({message: "Cadastro realizado com sucesso!"})
+
+})
+
+app.get("/usuarios",(req,res)=>{
+    res.json(usuarios)
 })
 
 app.listen(3000,()=>{
